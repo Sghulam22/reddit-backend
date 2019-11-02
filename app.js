7f97c29a525c10c0f4add6c00e669e78ec9e4f0d
Shabir Ghulam <sghulam@udel.edu>
2019-11-01 22:34:46 -0400

    update to router

--- app.js
+++ app.js
@@ -3,20 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/config');
 const parser = require('body-parser');
-
-app.use(parser.json());
+const post_route= require('./routers/posts');
+app.use(parser.json()); 
 
 //middle wear
-app.use('/',()=>{
-    console.log('homepage');
-});
+app.use('/posts',post_route);
 
-app.use('/posts',()=>{
-    console.log('posts');
-});
 
-//app.use(auth);
-
+//listen to server
+app.listen(3000);
 
 //routes
 app.get('/',(req,res)=>
@@ -28,14 +23,33 @@ app.get('/posts',(req,res) => {
     res.send ('we are in post');
 });
 
-mongoose.connect(config.database, { useMongoClient: true },()=>
-{
-    console.log('connected to db')
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.Promise = global.Promise   
+const uri = "mongodb+srv://test123:<qwerty66>@cluster0-ywbpt.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
-// mongoose.connect('mongodb+srv://test123:qwerty.66@cluster0-ywbpt.mongodb.net/test?retryWrites=true&w=majority',() => 
-// console.log('connected to db'),
-// useMongoClient: true
-// );
+
+
+// mongoose.set('debug', true);
+// mongoose.Promise = global.Promise
+// mongoose.connect(config.database, { useMongoClient: true },()=>
+// {
+//     console.log('connected to db')
+// });
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 //listen to server
-app.listen(3000);
+//app.listen(3000);
--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://test123:qwerty.66@cluster0-ywbpt.mongodb.net/test?retryWrites=true&w=majority',
+    database: 'mongodb://tdsfdsest123:<password>@cluster0-ywbpt.mongodb.net/test?retryWrites=true&w=majoritymongodb://test123:qwerty.66@cluster0-ywbpt.mongodb.net/test?retryWrites=true&w=majority',
     'port': 3000,
 }
